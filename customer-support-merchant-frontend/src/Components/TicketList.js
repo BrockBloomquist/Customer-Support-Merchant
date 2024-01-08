70e79954b850b82bdc67fd879775bfed17afd511
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2024-01-08 16:56:02 -0600

    Fixed misspelling

--- customer-support-merchant-frontend/src/Components/TicketList.js
+++ customer-support-merchant-frontend/src/Components/TicketList.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 export default function TicketList() {
   const url = "http://localhost:3005/tickets";
+  const [isLoading, setIsLoading] = useState(false);
   const [tickets, setTickets] = useState([]);
   const [ticket, setTicket] = useState({
     id: "",
@@ -14,31 +15,27 @@ export default function TicketList() {
     date: "",
   });
   useEffect(() => {
+    async function getTicketList() {
+      setIsLoading(true);
+      fetch(url, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          setTicket({
+            id: json[0].id,
+            fullName: json[0].fullName,
+            email: json[0].email,
+            description: json[0].details,
+            type: json[0].ticketType,
+            date: json[0].created,
+          });
+          setTickets([...tickets, ticket]);
+          console.log(ticket);
+        });
+    }
     getTicketList();
   }, []);
-  function getTicketList() {
-    fetch(url, {
-      mehtod: "GET",
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        setTicket({
-          ticket,
-          id: json[0].id,
-          fullName: json[0].fullName,
-          email: json[0].email,
-          description: json[0].details,
-          type: json[0].ticketType,
-          date: json[0].created,
-        });
-        setTickets([...tickets, ticket]);
-        console.log(tickets);
-      });
-  }
-  return (
-    <>
-      <Button onClick={getTicketList}>Click Me!</Button>
-    </>
-  );
+  return <></>;
 }
