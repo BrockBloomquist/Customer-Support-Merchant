8e43f1d4a3f62ae761e9abe89d2abad05e79670b
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2024-01-09 23:02:46 -0600

    Created a new Component for the Ticket View

--- customer-support-merchant-frontend/src/App.js
+++ customer-support-merchant-frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./Components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TicketList from "./Components/TicketList";
+import TicketView from "./Components/TicketView";
 
 function App() {
   return (
     <>
       <Header />
       <TicketList />
+      <TicketView />
     </>
   );
 }
---
+++ customer-support-merchant-frontend/src/Components/TicketView.js
@@ -0,0 +1,13 @@
+export default function TicketView() {
+  return (
+    <h1
+      style={{
+        position: "relative",
+        left: "50%",
+        top: "500px",
+      }}
+    >
+      HERE IS THE TICKET
+    </h1>
+  );
+}
